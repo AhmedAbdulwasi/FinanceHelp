0b07daa0318c2aa0022e708284792587e487f74f
Ahmed Bein <98428365+AhmedAbdulwasi@users.noreply.github.com>
2024-07-17 12:26:35 -0400

    Update stocks.py

--- venv/Routes/stocks.py
+++ venv/Routes/stocks.py
@@ -14,15 +14,14 @@ def handle_stocks():
         new_stocks = Stock(symbol=data['symbol'])
         db.session.add(new_stocks)
         db.session.commit()
-        return jsonify({"message": "Stock added to watchlist"}), 201
+        return jsonify({"message": "Stock added to the list"}), 201
     elif request.method == 'GET':
         stocks = Stock.query.all()
         stockdata = []
-        for stock in stocks:
+        for stock in stocks: # Found this info from https://www.alphavantage.co/documentation/
             url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock.symbol}&interval=5min&apikey={API_KEY}'
             r = requests.get(url)
             data = r.json()['Global Quote']
-
             stockdata.append({'symbol': stock.symbol, 'high': data['03. high'], 'low': data['04. low'], 'price': data['05. price'], 'volume': data['06. volume'], 'change_percent': data['10. change percent']})
 
         return jsonify(stockdata), 200
