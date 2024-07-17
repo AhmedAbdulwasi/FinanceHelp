9d1cab6448e2fb27b4b9b0d5ac4a73388f402996
Ahmed Bein <98428365+AhmedAbdulwasi@users.noreply.github.com>
2024-07-17 12:31:14 -0400

    Update models.py

--- venv/models.py
+++ venv/models.py
@@ -5,15 +5,15 @@ db = SQLAlchemy()
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
-    category = db.Column(db.String(64), nullable=False)
-    description = db.Column(db.String(256))
+    category = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200))
     date = db.Column(db.Date, nullable=False)
 
 class Income(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
-    source = db.Column(db.String(64), nullable=False)
-    description = db.Column(db.String(256))
+    source = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200))
     date = db.Column(db.Date, nullable=False)
 
 class Stock(db.Model):
